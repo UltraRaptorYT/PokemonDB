5e7638d89f818bb35d3798067b82f8e56d9bba4c
UltraRaptor <sohhongyu@gmail.com>
2023-04-24 11:33:47 +0800

    Update pokemon.csv and include scrapper app

---
+++ test.py
@@ -0,0 +1,240 @@
+string = """0906	Sprigatito	Sprigatito
+Grass
+#0907	Floragato	Floragato
+Grass
+#0908	Meowscarada	Meowscarada
+Grass	Dark
+#0909	Fuecoco	Fuecoco
+Fire
+#0910	Crocalor	Crocalor
+Fire
+#0911	Skeledirge	Skeledirge
+Fire	Ghost
+#0912	Quaxly	Quaxly
+Water
+#0913	Quaxwell	Quaxwell
+Water
+#0914	Quaquaval	Quaquaval
+Water	Fighting
+#0915	Lechonk	Lechonk
+Normal
+#0916	Oinkologne	Oinkologne
+Normal
+#0917	Tarountula	Tarountula
+Bug
+#0918	Spidops	Spidops
+Bug
+#0919	Nymble	Nymble
+Bug
+#0920	Lokix	Lokix
+Bug	Dark
+#0921	Pawmi	Pawmi
+Electric
+#0922	Pawmo	Pawmo
+Electric	Fighting
+#0923	Pawmot	Pawmot
+Electric	Fighting
+#0924	Tandemaus	Tandemaus
+Normal
+#0925	Maushold	Maushold
+Normal
+#0926	Fidough	Fidough
+Fairy
+#0927	Dachsbun	Dachsbun
+Fairy
+#0928	Smoliv	Smoliv
+Grass	Normal
+#0929	Dolliv	Dolliv
+Grass	Normal
+#0930	Arboliva	Arboliva
+Grass	Normal
+#0931	Squawkabilly	Squawkabilly
+Normal	Flying
+#0932	Nacli	Nacli
+Rock
+#0933	Naclstack	Naclstack
+Rock
+#0934	Garganacl	Garganacl
+Rock
+#0935	Charcadet	Charcadet
+Fire
+#0936	Armarouge	Armarouge
+Fire	Psychic
+#0937	Ceruledge	Ceruledge
+Fire	Ghost
+#0938	Tadbulb	Tadbulb
+Electric
+#0939	Bellibolt	Bellibolt
+Electric
+#0940	Wattrel	Wattrel
+Electric	Flying
+#0941	Kilowattrel	Kilowattrel
+Electric	Flying
+#0942	Maschiff	Maschiff
+Dark
+#0943	Mabosstiff	Mabosstiff
+Dark
+#0944	Shroodle	Shroodle
+Poison	Normal
+#0945	Grafaiai	Grafaiai
+Poison	Normal
+#0946	Bramblin	Bramblin
+Grass	Ghost
+#0947	Brambleghast	Brambleghast
+Grass	Ghost
+#0948	Toedscool	Toedscool
+Ground	Grass
+#0949	Toedscruel	Toedscruel
+Ground	Grass
+#0950	Klawf	Klawf
+Rock
+#0951	Capsakid	Capsakid
+Grass
+#0952	Scovillain	Scovillain
+Grass	Fire
+#0953	Rellor	Rellor
+Bug
+#0954	Rabsca	Rabsca
+Bug	Psychic
+#0955	Flittle	Flittle
+Psychic
+#0956	Espathra	Espathra
+Psychic
+#0957	Tinkatink	Tinkatink
+Fairy	Steel
+#0958	Tinkatuff	Tinkatuff
+Fairy	Steel
+#0959	Tinkaton	Tinkaton
+Fairy	Steel
+#0960	Wiglett	Wiglett
+Water
+#0961	Wugtrio	Wugtrio
+Water
+#0962	Bombirdier	Bombirdier
+Flying	Dark
+#0963	Finizen	Finizen
+Water
+#0964	Palafin	Palafin
+Zero Form	Water
+Palafin	Palafin
+Hero Form	Water
+#0965	Varoom	Varoom
+Steel	Poison
+#0966	Revavroom	Revavroom
+Steel	Poison
+#0967	Cyclizar	Cyclizar
+Dragon	Normal
+#0968	Orthworm	Orthworm
+Steel
+#0969	Glimmet	Glimmet
+Rock	Poison
+#0970	Glimmora	Glimmora
+Rock	Poison
+#0971	Greavard	Greavard
+Ghost
+#0972	Houndstone	Houndstone
+Ghost
+#0973	Flamigo	Flamigo
+Flying	Fighting
+#0974	Cetoddle	Cetoddle
+Ice
+#0975	Cetitan	Cetitan
+Ice
+#0976	Veluza	Veluza
+Water	Psychic
+#0977	Dondozo	Dondozo
+Water
+#0978	Tatsugiri	Tatsugiri
+Curly Form	Dragon	Water
+Tatsugiri	Tatsugiri
+Droopy Form	Dragon	Water
+Tatsugiri	Tatsugiri
+Stretchy Form	Dragon	Water
+#0979	Annihilape	Annihilape
+Fighting	Ghost
+#0980	Clodsire	Clodsire
+Poison	Ground
+#0981	Farigiraf	Farigiraf
+Normal	Psychic
+#0982	Dudunsparce	Dudunsparce
+Normal
+#0983	Kingambit	Kingambit
+Dark	Steel
+#0984	Great Tusk	Great Tusk
+Ground	Fighting
+#0985	Scream Tail	Scream Tail
+Fairy	Psychic
+#0986	Brute Bonnet	Brute Bonnet
+Grass	Dark
+#0987	Flutter Mane	Flutter Mane
+Ghost	Fairy
+#0988	Slither Wing	Slither Wing
+Bug	Fighting
+#0989	Sandy Shocks	Sandy Shocks
+Electric	Ground
+#0990	Iron Treads	Iron Treads
+Ground	Steel
+#0991	Iron Bundle	Iron Bundle
+Ice	Water
+#0992	Iron Hands	Iron Hands
+Fighting	Electric
+#0993	Iron Jugulis	Iron Jugulis
+Dark	Flying
+#0994	Iron Moth	Iron Moth
+Fire	Poison
+#0995	Iron Thorns	Iron Thorns
+Rock	Electric
+#0996	Frigibax	Frigibax
+Dragon	Ice
+#0997	Arctibax	Arctibax
+Dragon	Ice
+#0998	Baxcalibur	Baxcalibur
+Dragon	Ice
+#0999	Gimmighoul	Gimmighoul
+Chest Form	Ghost
+Gimmighoul	Gimmighoul
+Roaming Form	Ghost
+#1000	Gholdengo	Gholdengo
+Steel	Ghost
+#1001	Wo-Chien	Wo-Chien
+Dark	Grass
+#1002	Chien-Pao	Chien-Pao
+Dark	Ice
+#1003	Ting-Lu	Ting-Lu
+Dark	Ground
+#1004	Chi-Yu	Chi-Yu
+Dark	Fire
+#1005	Roaring Moon	Roaring Moon
+Dragon	Dark
+#1006	Iron Valiant	Iron Valiant
+Fairy	Fighting
+#1007	Koraidon	Koraidon
+Fighting	Dragon
+#1008	Miraidon	Miraidon
+Electric	Dragon
+#1009	Walking Wake	Walking Wake
+Water	Dragon
+#1010	Iron Leaves	Iron Leaves
+Grass	Psychic"""
+
+
+# # 0000	Ogerpon	Ogerpon
+# Unknown
+# # 0000	Okidogi	Okidogi
+# Unknown
+# # 0000	Munkidori	Munkidori
+# Unknown
+# # 0000	Fezandipiti	Fezandipiti
+# Unknown
+# # 0000	Terapagos	Terapagos
+# Unknown
+
+output = ""
+pokemonArr = []
+
+for pokemon in string.split("\n#"):
+  output += "\t".join(pokemon.split("\n")) + "\n"
+  pokemonArr.append("_".join(pokemon.split("\n")[0].split("\t")[1].split(" ")))
+
+print(output)
+print(pokemonArr)
